f0d8e17f86457d1366817b6cdca6585ff3c69661
RAKESH KUMAR <rakeshbimra@outlook.com>
2022-05-07 23:49:12 +0530

    Add project files.

---
+++ AzureCosmosDbRepositoryDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using AzureCosmosDbRepositoryDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CosmosRepository;
+
+namespace AzureCosmosDbRepositoryDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IRepository<Employee> _repository;
+
+        public EmployeeController(IRepositoryFactory factory)
+        {
+            _repository = factory.RepositoryOf<Employee>();
+        }
+
+        [HttpGet(Name = nameof(GetEmployees))]
+        public ValueTask<IEnumerable<Employee>> GetEmployees() =>
+            _repository.GetByQueryAsync(queryDefinition: new Microsoft.Azure.Cosmos.QueryDefinition("SELECT * FROM c"));
+
+        [HttpGet("{id}", Name = nameof(GetEmployeeById))]
+        public ValueTask<Employee> GetEmployeeById(string id) =>
+            _repository.GetAsync(id,"1");
+
+        [HttpPost]
+        public ValueTask<IEnumerable<Employee>> AddEmployees([FromBody] params Employee[] employees) =>
+            _repository.CreateAsync(employees);
+    }
+}
---
+++ AzureCosmosDbRepositoryDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.CosmosRepository;
+using Microsoft.Azure.CosmosRepository.Attributes;
+
+namespace AzureCosmosDbRepositoryDemo.Models
+{
+    [PartitionKeyPath("/employeeId")]
+    public class Employee : Item
+    {
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        protected override string GetPartitionKeyValue() => EmployeeId;
+    }
+}
